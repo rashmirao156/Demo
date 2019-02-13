906129c030c07f801a25844179d622b3d7881dc4
Rashmi R <rashmirao156@gmail.com>
2019-02-13 15:52:41 +0530

    adding adapter component

---
+++ src/main/java/com/deloitte/sample/integration/demo/adapter/configuration/AdapterConfiguration.java
@@ -0,0 +1,14 @@
+package com.deloitte.sample.integration.demo.adapter.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "integration.adapter.demo")
+public class AdapterConfiguration {
+
+    private String adapterInboundQueueUri;
+    private String adapterOutboundQueueUri;
+}
---
+++ src/main/java/com/deloitte/sample/integration/demo/adapter/route/AdapterRoute.java
@@ -0,0 +1,36 @@
+package com.deloitte.sample.integration.demo.adapter.route;
+
+import com.deloitte.sample.integration.demo.adapter.configuration.AdapterConfiguration;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdapterRoute extends RouteBuilder {
+  private static final String APP_NAME_JMS_HEADER = "APP_NAME_JMS_HEADER";
+  private static final String MESSAGE_VERSION_JMS_HEADER = "MESSAGE_VERSION_JMS_HEADER";
+  private static final String SERVICE_VERSION_JMS_HEADER = "SERVICE_VERSION_JMS_HEADER";
+  private static final String MESSAGE_TYPE_JMS_HEADER = "MESSAGE_TYPE_JMS_HEADER";
+
+  @Autowired AdapterConfiguration adapterConfiguration;
+
+  @Override
+  public void configure() throws Exception {
+
+    /* Route mocking Adapter behaviour .
+    consumes messages from inbound queue such as IBM MQ / activemq,
+     adds headers enabling tracking,
+     sends acknowledgement  that message was recieved,
+     forwards the message to outbound queue*/
+    from(adapterConfiguration.getAdapterInboundQueueUri())
+        // .to("drect:recievedtrackinguri")
+        // .to("direct:ack-route")
+        .setHeader(APP_NAME_JMS_HEADER, constant("accelerator-adapter"))
+        .setHeader(MESSAGE_VERSION_JMS_HEADER, constant("1.0.0"))
+        .setHeader(SERVICE_VERSION_JMS_HEADER, constant("1.0.0"))
+        .setHeader(MESSAGE_TYPE_JMS_HEADER, constant("raw"))
+        .to(adapterConfiguration.getAdapterOutboundQueueUri())
+    // .to("drect:senttrackinguri")
+    ;
+  }
+}
